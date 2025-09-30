f7e37095c31d8a93dbf6075e8ebe37ee5ae4b402
caBreRaS14 <97133878+caBreRaS14@users.noreply.github.com>
2025-09-30 17:20:59 -0500

    Versión lista para Renrrrr

--- configuracion/wsgi.py
+++ configuracion/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configuracion.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miportafolio.settings')
 
 application = get_wsgi_application()
